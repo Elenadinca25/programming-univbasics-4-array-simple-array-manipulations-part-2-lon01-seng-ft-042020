580017ca38c92bc7d934eefd815aa569ccf842b2
Elena Dinca <elenadinca25@gmail.com>
2020-04-02 15:07:09 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,10 +2,17 @@ def using_concat(array1, array2)
   array1.concat(array2)
 end
 
+
  def using_insert(array, element)
    array.insert(4,'Python')
  end
 
+
 def using_uniq(array)
   array.uniq
+end
+
+
+def using_flatten(array)
+  array.flatten
 end
